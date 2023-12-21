f91ecc294e02fa8e1c0008fb708449e03e9a3ee1
maumia5 <manueldias@tutamail.com>
2023-12-21 15:05:54 +0000

    Código completo, se conseguirmos extra de grafico cotovelo era fixe

--- proj1/g1.r
+++ proj1/g1.r
@@ -8,20 +8,60 @@ library(DescTools)
 library(MASS)
 
 ####1####
-###All###
 dat = as.data.frame(BudgetItaly)
 dat <- subset(dat, select = -4)
-dat_noyear <- subset(dat, select = -7)
-dat_sum <- summary(dat)
-dat
-
-#3D exploded pie chart with all the years
-pie3D(as.vector(table(BudgetItaly$year)),labels=names(table(BudgetItaly$year)),
-      explode=0.1, main="BudgetItaly years",labelcex=1.0)
 
 ###Year73###
 dat_73 <- subset(dat, dat$year == "73")
 dat_73 <- subset(dat_73, select = -7)
+
+###Resume of data###
+describe(dat_73)
+
+#Winsorized Mean#
+dat_73_win_wfood = round(winmean(dat_73$wfood, trim = 0.2), digits = 4)
+dat_73_win_whouse = round(winmean(dat_73$whouse, trim = 0.2), digits = 4)
+dat_73_win_wmisc = round(winmean(dat_73$wmisc, trim = 0.2), digits = 4)
+dat_73_win_phouse = round(winmean(dat_73$phouse, trim = 0.2), digits = 4)
+dat_73_win_pmisc = round(winmean(dat_73$pmisc, trim = 0.2), digits = 4)
+dat_73_win_tt = round(winmean(dat_73$totexp, trim = 0.2), digits = 4)
+dat_73_win_income = round(winmean(dat_73$income, trim = 0.2), digits = 4)
+dat_73_win_size = round(winmean(dat_73$size, trim = 0.2), digits = 4)
+dat_73_win_pct = round(winmean(dat_73$pct, trim = 0.2), digits = 4)
+
+#Variance#
+dat_73_var = round(apply(dat_73, 2, var), digits = 4)
+dat_73_vartot = round(sum(dat_73_var), digits = 4)
+
+#Covariance#
+dat_73_cov = round(cov(dat_73), digits = 4)
+
+#mad variable#
+dat_73_mad_wfood = round(mad(dat_73$wfood), digits = 4)
+dat_73_mad_whouse = round(mad(dat_73$whouse), digits = 4)
+dat_73_mad_wmisc = round(mad(dat_73$wmisc), digits = 4)
+dat_73_mad_phouse = round(mad(dat_73$phouse), digits = 4)
+dat_73_mad_pmisc = round(mad(dat_73$pmisc), digits = 4)
+dat_73_mad_totexp = round(mad(dat_73$totexp), digits = 4)
+dat_73_mad_income = round(mad(dat_73$income), digits = 4)
+dat_73_mad_size = round(mad(dat_73$size), digits = 4)
+dat_73_mad_pct = round(mad(dat_73$pct), digits = 4)
+
+#Mahalabonis Distance#
+dat_73_maha = round(mahalanobis(dat_73, dat_73_mean, dat_73_cov), digits = 4)
+
+###Tables###
+year73_mat <- data.frame(
+  Insorized_Mean = c(dat_73_win_wfood, dat_73_win_whouse, dat_73_win_wmisc, 
+                     dat_73_win_phouse, dat_73_win_pmisc, dat_73_win_tt, 
+                     dat_73_win_income, dat_73_win_size, dat_73_win_pct),
+  Variance = cbind(data.matrix(dat_73_var)),
+  Mad = c(dat_73_mad_wfood, dat_73_mad_whouse, dat_73_mad_wmisc,
+          dat_73_mad_phouse, dat_73_mad_pmisc, dat_73_mad_totexp,
+          dat_73_mad_income, dat_73_mad_size, dat_73_mad_pct))
+year73_mat
+
+###Graphics###
 
 ##Boxplot of all the variables pf year 73 except pfood
 # Create boxplots for each variable in the data frame
@@ -38,56 +78,9 @@ for (col_name in names(dat_73)) {
 }
 dev.off()
 
-##Mean#
-dat_73_mean <- colMeans(dat_73)
+###
+pairs.panels(dat_73)
 
-#Trimmed Mean#
-dat_73_trim_wfood = mean(dat_73$wfood, trim = 0.2)
-dat_73_trim_whouse = mean(dat_73$whouse, trim = 0.2)
-dat_73_trim_wmisc = mean(dat_73$wmisc, trim = 0.2)
-dat_73_trim_phouse = mean(dat_73$phouse, trim = 0.2)
-dat_73_trim_pmisc = mean(dat_73$pmisc, trim = 0.2)
-dat_73_trim_tt = mean(dat_73$totexp, trim = 0.2)
-dat_73_trim_income = mean(dat_73$income, trim = 0.2)
-dat_73_trim_size = mean(dat_73$size, trim = 0.2)
-dat_73_trim_pct = mean(dat_73$pct, trim = 0.2)
-
-#Winsorized Mean#
-dat_73_win_wfood = winmean(dat_73$wfood, trim = 0.2)
-dat_73_win_whouse = winmean(dat_73$whouse, trim = 0.2)
-dat_73_win_wmisc = winmean(dat_73$wmisc, trim = 0.2)
-dat_73_win_phouse = winmean(dat_73$phouse, trim = 0.2)
-dat_73_win_pmisc = winmean(dat_73$pmisc, trim = 0.2)
-dat_73_win_tt = winmean(dat_73$totexp, trim = 0.2)
-dat_73_win_year = winmean(dat_73$year, trim = 0.2)
-dat_73_win_income = winmean(dat_73$income, trim = 0.2)
-dat_73_win_size = winmean(dat_73$size, trim = 0.2)
-dat_73_win_pct = winmean(dat_73$pct, trim = 0.2)
-#Median#quando distribuiçao e simetrica media e mediana coincide
-dat_73_median <- sapply(dat_73, median)
-
-#Variance#
-dat_73_var = apply(dat_73, 2, var)
-dat_73_vartot = sum(dat_73_var)
-dat_73_vartot#referir no relatorio 
-#Covariance#
-dat_73_cov = round(cov(dat_73), digits = 9)
-dat_73_gen_var = det(dat_73_var/dat_73_cov)
-
-#mad variable#
-dat_73_mad_wfood = mad(dat_73$wfood)
-dat_73_mad_whouse = mad(dat_73$whouse)
-dat_73_mad_wmisc = mad(dat_73$wmisc)
-dat_73_mad_phouse = mad(dat_73$phouse)
-dat_73_mad_pmisc = mad(dat_73$pmisc)
-dat_73_mad_totexp = mad(dat_73$totexp)
-dat_73_mad_year = mad(dat_73$year)
-dat_73_mad_income = mad(dat_73$income)
-dat_73_mad_size = mad(dat_73$size)
-dat_73_mad_pct = mad(dat_73$pct)
-
-#Mahalabonis Distance#
-dat_73_maha = mahalanobis(dat_73, dat_73_mean, dat_73_cov)
 #Boxplot of Mahalanobis Distance
 pdf("mahalanobis_distances3.pdf", width = 8, height = 5)
 plot(
@@ -104,12 +97,6 @@ abline(
   lty = 2
 )
 dev.off()
-
-###Year73###
-
-
-
-
 ####2####
 #Original Scale#Classical Sample Covariance Estimate#
 dat_73_budget.pca <- prcomp(dat_73)
@@ -119,9 +106,27 @@ dat_73_stand <- scale(dat_73)
 dat_73_pca_stand <- prcomp(dat_73_stand)
 
 ##teste b adicionar graf cotovelo
-summary(dat_73_pca_stand)
 summary(dat_73_budget.pca)
+summary(dat_73_pca_stand)
 
+##biplot
+#A#
+score <- dat_73_budget.pca$x
+plot(score[,1], score[,2], xlab = "PC1", ylab = "PC2", type = "n", xlim = c(min(score[,1]),
+                                                                            max(score[,1])), ylim = c(min(score[,2]), max(score[,2])))
+text(score[,1],score[,2], rownames(score),
+     col="blue",cex=0.7)
+abline(h=mean(score[,2]),col="green")
+abline(v=mean(score[,1]),col="green")
+
+#B#
+score_std <- dat_73_pca_stand$x
+plot(score_std[,1], score_std[,2], xlab = "PC1", ylab = "PC2", type = "n", xlim = c(min(score_std[,1]),
+                                                                                    max(score_std[,1])), ylim = c(min(score_std[,2]), max(score_std[,2])))
+text(score_std[,1],score_std[,2], rownames(score_std),
+     col="blue",cex=0.7)
+abline(h=mean(score_std),col="green")
+abline(v=mean(score_std[,1]),col="green")
 
 ####3####
 ##ALL##
@@ -132,8 +137,6 @@ for (row_index in 1:5){
     dat_p3[row_index, col_index] <- dat_p3[row_index, col_index] * 0.01
   }
 }
-#Classical PCA#
-dat_p3.pca <- prcomp(dat_p3)
 
 ##Year 73##
 dat_73_p3 <- dat_73
@@ -145,6 +148,7 @@ for (row_index in 1:5) {
 }
 #Classical PCA#
 dat_73_p3.pca <- prcomp(dat_73_p3)
+summary(dat_73_budget.pca)
 summary(dat_73_p3.pca)
 #3 - biplots
 score_p3.pca <- dat_73_p3.pca$x
@@ -156,5 +160,6 @@ abline(h=mean(score_p3.pca[,2]),col="green")
 abline(v=mean(score_p3.pca[,1]),col="green")
 
 ##Robust PCA on the MCD estimate, terminar
-dat_73.mcd_pca <- eigen(cov.rob(dat_73_p3)$cov)
-dat_73.mcd_pca
+dat_pca_robust <- PcaCov(dat_73_p3)
+dat_pca_robust
+biplot(dat_pca_robust, scale = 0, cex = 0.6)
