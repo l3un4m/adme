e7cd69d0710a117364fbd8ed61bfe3ea6600cc81
maumia5 <manueldias@tutamail.com>
2024-01-04 14:45:28 +0000

    1, 2a) e 2c) feitos, falta dar resposta bonita.

--- proj1/g1.r
+++ proj1/g1.r
@@ -1,90 +1,148 @@
+#install.packages('Ecdat')
 library('Ecdat')
-library(rrcov)
-library(plotrix)
-library(stats)
-library(WRS2)
-library(psych)
-library(DescTools)
-library(MASS)
+##data(package = "Ecdat")
 
-####1####
+#@@@1@@@#
+#####All#####
 dat = as.data.frame(BudgetItaly)
 dat <- subset(dat, select = -4)
+dat_noyear <- subset(dat, select = -7)
+dat_sum <- summary(dat)
+
+#Mean#
+dat_mean <- colMeans(dat)
+dat_mean
+dat_noyear_mean <- colMeans(dat_noyear) #To create a table to compare we need to remove the year
+
+#Trimmed Mean#
+dat_trim_wfood = mean(dat$wfood, trim = 0.2)
+dat_trim_whouse = mean(dat$whouse, trim = 0.2)
+dat_trim_wmisc = mean(dat$wmisc, trim = 0.2)
+dat_trim_phouse = mean(dat$phouse, trim = 0.2)
+dat_trim_pmisc = mean(dat$pmisc, trim = 0.2)
+dat_trim_tt = mean(dat$totexp, trim = 0.2)
+dat_trim_year = mean(dat$year, trim = 0.2)
+dat_trim_income = mean(dat$income, trim = 0.2)
+dat_trim_size = mean(dat$size, trim = 0.2)
+dat_trim_pct = mean(dat$pct, trim = 0.2)
+
+#Winsorized Mean#
+library(WRS2)
+dat_win_wfood = winmean(dat$wfood, trim = 0.2)
+dat_win_whouse = winmean(dat$whouse, trim = 0.2)
+dat_win_wmisc = winmean(dat$wmisc, trim = 0.2)
+dat_win_phouse = winmean(dat$phouse, trim = 0.2)
+dat_win_pmisc = winmean(dat$pmisc, trim = 0.2)
+dat_win_tt = winmean(dat$totexp, trim = 0.2)
+dat_win_year = winmean(dat$year, trim = 0.2)
+dat_win_income = winmean(dat$income, trim = 0.2)
+dat_win_size = winmean(dat$size, trim = 0.2)
+dat_win_pct = winmean(dat$pct, trim = 0.2)
+
+#Median#
+dat_median <- sapply(dat, median)
+
+#Variance#
+dat_var = apply(dat, 2, var)
+datnoyear_var <- apply(dat_noyear, 2, var) #To create a table to compare we need to remove the year
+dat_vartot = sum(dat_var)
+
+#Covariance#
+dat_cov = round(cov(dat), digits = 9)
+dat_gen_var = det(dat_var/dat_cov)
+
+#mad variable#
+library(stats)
+dat_mad_wfood = mad(dat$wfood)
+dat_mad_whouse = mad(dat$whouse)
+dat_mad_wmisc = mad(dat$wmisc)
+dat_mad_phouse = mad(dat$phouse)
+dat_mad_pmisc = mad(dat$pmisc)
+dat_mad_totexp = mad(dat$totexp)
+dat_mad_year = mad(dat$year)
+dat_mad_income = mad(dat$income)
+dat_mad_size = mad(dat$size)
+dat_mad_pct = mad(dat$pct)
+
+#Mahalabonis Distance#
+dat_maha = mahalanobis(dat, dat_mean, dat_cov)
+
+#####All#####
+
 
 ###Year73###
 dat_73 <- subset(dat, dat$year == "73")
 dat_73 <- subset(dat_73, select = -7)
-
-###Resume of data###
-describe(dat_73)
-
-#Winsorized Mean#
-dat_73_win_wfood = round(winmean(dat_73$wfood, trim = 0.2), digits = 4)
-dat_73_win_whouse = round(winmean(dat_73$whouse, trim = 0.2), digits = 4)
-dat_73_win_wmisc = round(winmean(dat_73$wmisc, trim = 0.2), digits = 4)
-dat_73_win_phouse = round(winmean(dat_73$phouse, trim = 0.2), digits = 4)
-dat_73_win_pmisc = round(winmean(dat_73$pmisc, trim = 0.2), digits = 4)
-dat_73_win_tt = round(winmean(dat_73$totexp, trim = 0.2), digits = 4)
-dat_73_win_income = round(winmean(dat_73$income, trim = 0.2), digits = 4)
-dat_73_win_size = round(winmean(dat_73$size, trim = 0.2), digits = 4)
-dat_73_win_pct = round(winmean(dat_73$pct, trim = 0.2), digits = 4)
-
-#Variance#
-dat_73_var = round(apply(dat_73, 2, var), digits = 4)
-dat_73_vartot = round(sum(dat_73_var), digits = 4)
-
-#Covariance#
-dat_73_cov = round(cov(dat_73), digits = 4)
-
-#mad variable#
-dat_73_mad_wfood = round(mad(dat_73$wfood), digits = 4)
-dat_73_mad_whouse = round(mad(dat_73$whouse), digits = 4)
-dat_73_mad_wmisc = round(mad(dat_73$wmisc), digits = 4)
-dat_73_mad_phouse = round(mad(dat_73$phouse), digits = 4)
-dat_73_mad_pmisc = round(mad(dat_73$pmisc), digits = 4)
-dat_73_mad_totexp = round(mad(dat_73$totexp), digits = 4)
-dat_73_mad_income = round(mad(dat_73$income), digits = 4)
-dat_73_mad_size = round(mad(dat_73$size), digits = 4)
-dat_73_mad_pct = round(mad(dat_73$pct), digits = 4)
-
-#Mahalabonis Distance#
-dat_73_maha = round(mahalanobis(dat_73, dat_73_mean, dat_73_cov), digits = 4)
-
-###Tables###
-year73_mat <- data.frame(
-  Insorized_Mean = c(dat_73_win_wfood, dat_73_win_whouse, dat_73_win_wmisc, 
-                     dat_73_win_phouse, dat_73_win_pmisc, dat_73_win_tt, 
-                     dat_73_win_income, dat_73_win_size, dat_73_win_pct),
-  Variance = cbind(data.matrix(dat_73_var)),
-  Mad = c(dat_73_mad_wfood, dat_73_mad_whouse, dat_73_mad_wmisc,
-          dat_73_mad_phouse, dat_73_mad_pmisc, dat_73_mad_totexp,
-          dat_73_mad_income, dat_73_mad_size, dat_73_mad_pct))
-year73_mat
-
-###Graphics###
-
-##Boxplot of all the variables pf year 73 except pfood
-# Create boxplots for each variable in the data frame
 pdf("boxplots55.pdf", width = 10, height = 5)
 par(mfrow = c(1, ncol(dat_73)))
 for (col_name in names(dat_73)) {
-  boxplot(
-    dat_73[[col_name]],
-    main = col_name, col = "lightpink",
-    border = "black",
-    notch = TRUE
-  )
-  rm(col_name)
+   boxplot(
+   dat_73[[col_name]],
+   main = col_name, col = "lightpink",
+   border = "black",
+   notch = TRUE
+)
+rm(col_name)
 }
 dev.off()
 
-###
-pairs.panels(dat_73)
+##Mean#
+dat_73_mean <- colMeans(dat_73)
 
+#Trimmed Mean#
+dat_73_trim_wfood = mean(dat_73$wfood, trim = 0.2)
+dat_73_trim_whouse = mean(dat_73$whouse, trim = 0.2)
+dat_73_trim_wmisc = mean(dat_73$wmisc, trim = 0.2)
+dat_73_trim_phouse = mean(dat_73$phouse, trim = 0.2)
+dat_73_trim_pmisc = mean(dat_73$pmisc, trim = 0.2)
+dat_73_trim_tt = mean(dat_73$totexp, trim = 0.2)
+dat_73_trim_income = mean(dat_73$income, trim = 0.2)
+dat_73_trim_size = mean(dat_73$size, trim = 0.2)
+dat_73_trim_pct = mean(dat_73$pct, trim = 0.2)
+
+#Winsorized Mean#
+library(WRS2)
+dat_73_win_wfood = winmean(dat_73$wfood, trim = 0.2)
+dat_73_win_whouse = winmean(dat_73$whouse, trim = 0.2)
+dat_73_win_wmisc = winmean(dat_73$wmisc, trim = 0.2)
+dat_73_win_phouse = winmean(dat_73$phouse, trim = 0.2)
+dat_73_win_pmisc = winmean(dat_73$pmisc, trim = 0.2)
+dat_73_win_tt = winmean(dat_73$totexp, trim = 0.2)
+dat_73_win_year = winmean(dat_73$year, trim = 0.2)
+dat_73_win_income = winmean(dat_73$income, trim = 0.2)
+dat_73_win_size = winmean(dat_73$size, trim = 0.2)
+dat_73_win_pct = winmean(dat_73$pct, trim = 0.2)
+
+#Median#
+dat_73_median <- sapply(dat_73, median)
+
+#Variance#
+dat_73_var = apply(dat_73, 2, var)
+dat_73_vartot = sum(dat_73_var)
+
+#Covariance#
+dat_73_cov = round(cov(dat_73), digits = 9)
+dat_73_gen_var = det(dat_73_var/dat_73_cov)
+
+#mad variable#
+library(stats)
+dat_73_mad_wfood = mad(dat_73$wfood)
+dat_73_mad_whouse = mad(dat_73$whouse)
+dat_73_mad_wmisc = mad(dat_73$wmisc)
+dat_73_mad_phouse = mad(dat_73$phouse)
+dat_73_mad_pmisc = mad(dat_73$pmisc)
+dat_73_mad_totexp = mad(dat_73$totexp)
+dat_73_mad_year = mad(dat_73$year)
+dat_73_mad_income = mad(dat_73$income)
+dat_73_mad_size = mad(dat_73$size)
+dat_73_mad_pct = mad(dat_73$pct)
+
+#Mahalabonis Distance#
+dat_73_maha = mahalanobis(dat_73, dat_73_mean, dat_73_cov)
 #Boxplot of Mahalanobis Distance
 pdf("mahalanobis_distances3.pdf", width = 8, height = 5)
 plot(
-  dat_73_maha,
+  mahalanobis_dist,
   pch = 16,
   col = "purple",
   main = "Mahalanobis Distances",
@@ -97,69 +155,70 @@ abline(
   lty = 2
 )
 dev.off()
-####2####
-#Original Scale#Classical Sample Covariance Estimate#
+
+
+###Year73###
+
+###Graph###
+mean_mat <- data.frame(cbind(data.matrix(dat_73_mean), data.matrix(dat_noyear_mean)))
+mean_mat
+var_mat <- data.frame(cbind(data.matrix(dat_73_var), data.matrix(datnoyear_var)))
+var_mat
+
+
+
+#@@@2@@@#
+library(rrcov)
+#Original Scale#
+dat_budget.pca <- prcomp(dat)
+dat_budget.pca
 dat_73_budget.pca <- prcomp(dat_73)
+dat_73_budget.pca
+
+#Classical Sample Covariance Estimate#ATENÇÂO
+dat_cc <- CovClassic(dat)
+dat_cc.pca <- prcomp(dat_cc)
+dat_73_cc <- CovClassic(dat_73)
+dat_73_cc.pca <- prcomp(dat_73_cc)
+
+
+#@@@B@@@#
 
 #Standardized Variables#
+dat_stand <- scale(dat)
+dat_pca_stand <- prcomp(dat_stand)
+summary(dat_pca_stand)
 dat_73_stand <- scale(dat_73)
 dat_73_pca_stand <- prcomp(dat_73_stand)
-
-##teste b adicionar graf cotovelo
-summary(dat_73_budget.pca)
 summary(dat_73_pca_stand)
 
-##biplot
-#A#
-score <- dat_73_budget.pca$x
-plot(score[,1], score[,2], xlab = "PC1", ylab = "PC2", type = "n", xlim = c(min(score[,1]),
-                                                                            max(score[,1])), ylim = c(min(score[,2]), max(score[,2])))
-text(score[,1],score[,2], rownames(score),
-     col="blue",cex=0.7)
-abline(h=mean(score[,2]),col="green")
-abline(v=mean(score[,1]),col="green")
 
-#B#
-score_std <- dat_73_pca_stand$x
-plot(score_std[,1], score_std[,2], xlab = "PC1", ylab = "PC2", type = "n", xlim = c(min(score_std[,1]),
-                                                                                    max(score_std[,1])), ylim = c(min(score_std[,2]), max(score_std[,2])))
-text(score_std[,1],score_std[,2], rownames(score_std),
-     col="blue",cex=0.7)
-abline(h=mean(score_std),col="green")
-abline(v=mean(score_std[,1]),col="green")
+#Percentage of Total Variance#
+eigen_values <- eigen(cov(dat))$values
+dat_var_per <- eigen_values / sum(eigen_values) * 100
+dat_var_per
 
-####3####
-##ALL##
-dat_p3 <- dat_73
+eigen_values_std <- eigen(cor(dat))$values
+dat_vat_per_std <- eigen_values_std / sum(eigen_values_std) * 100
+dat_vat_per_std
+
+library(psy)
+dat_cc
+third_component <- eigen_vectors[, 3]
+scree.plot(dat_stand,title="Stdardized",type="R")
+scree.plot(third_component, title= "Classic", type="R")
+
+#@@@3@@@#
+dat_p3 <- dat
+
 #Function to multiply the values of the 5 first rows by 0.01 as it's asked
-for (row_index in 1:5){
-  for (col_index in 1:9){
+for (row_index in 1:5) {
+  for (col_index in 1:ncol(dat_p3)) {
     dat_p3[row_index, col_index] <- dat_p3[row_index, col_index] * 0.01
   }
 }
 
-##Year 73##
-dat_73_p3 <- dat_73
-#Function to multiply the values of the 5 first rows by 0.01 as it's asked
-for (row_index in 1:5) {
-  for (col_index in 1:9) {
-    dat_73_p3[row_index, col_index] <- dat_73_p3[row_index, col_index] * 0.01
-  }
-}
 #Classical PCA#
-dat_73_p3.pca <- prcomp(dat_73_p3)
-summary(dat_73_budget.pca)
-summary(dat_73_p3.pca)
-#3 - biplots
-score_p3.pca <- dat_73_p3.pca$x
-plot(score_p3.pca[,1], score_p3.pca[,2], xlab = "PC1", ylab = "PC2", type = "n", xlim = c(min(score_p3.pca[,1]),
-  max(score_p3.pca[,1])), ylim = c(min(score_p3.pca[,2]), max(score_p3.pca[,2])))
-text(score_p3.pca[,1],score_p3.pca[,2], rownames(score_p3.pca),
-  col="blue",cex=0.7)
-abline(h=mean(score_p3.pca[,2]),col="green")
-abline(v=mean(score_p3.pca[,1]),col="green")
+dat_p3.pca <- prcomp(dat_p3)
 
-##Robust PCA on the MCD estimate, terminar
-dat_pca_robust <- PcaCov(dat_73_p3)
-dat_pca_robust
-biplot(dat_pca_robust, scale = 0, cex = 0.6)
+#Robust PCA#ATENÇÂO
--- proj2/p2.r
+++ proj2/p2.r
@@ -1,5 +1,11 @@
 # Load necessary library
-library(ISLR)
+library(rrcov)
+library(plotrix)
+library(stats)
+library(WRS2)
+library(psych)
+library(DescTools)
+library(MASS)
 
 # Load Auto dataset
 data("Auto")
@@ -7,41 +13,150 @@ data("Auto")
 # Select subset from observation 1 to 50
 auto_subset <- Auto[1:50, 1:8]
 
-# Summary statistics
-summary(auto_subset)
+# Resume statistics
+tab_describe <-describe(auto_subset)
 
-# Pairwise scatterplot matrix
-pairs(auto_subset)
-# Fit a multiple linear regression model
-lm_model <- lm(mpg ~ ., data = auto_subset)
+# Mean
+auto_mean <- apply(auto_subset, 2, mean)
 
-# Summary of the regression
-summary(lm_model)
-# Residuals vs Fitted plot
-plot(lm_model, which = 1)
+#Winsorized Mean#
+auto_win_mpg = round(winmean(auto_subset$mpg, trim = 0.2), digits = 4)
+auto_win_cylinders = round(winmean(auto_subset$cylinders, trim = 0.2), digits = 4)
+auto_win_displacement = round(winmean(auto_subset$displacement, trim = 0.2), digits = 4)
+auto_win_horsepower = round(winmean(auto_subset$horsepower, trim = 0.2), digits = 4)
+auto_win_weight = round(winmean(auto_subset$weight, trim = 0.2), digits = 4)
+auto_win_acceleration= round(winmean(auto_subset$acceleration, trim = 0.2), digits = 4)
+auto_win_year = round(winmean(auto_subset$cylinders, trim = 0.2), digits = 4)
+auto_win_origin = round(winmean(auto_subset$origin, trim = 0.2), digits = 4)
 
-# Normal Q-Q plot
-plot(lm_model, which = 2)
+#Variance#
+auto_var = round(apply(auto_subset, 2, var), digits = 4)
+auto_vartot = round(sum(auto_subset), digits = 4)
 
-# Cook's distance plot
-plot(lm_model, which = 4)
+#Covariance#
+auto_cov = round(cov(auto_subset), digits = 4)
 
-# Identify influential observations
-influential_obs <- which( cooks.distance(lm_model) > 4 / length(auto_subset$mpg) )
+#Mahalabonis Distance#
+auto_maha = round(mahalanobis(auto_subset, auto_mean, auto_cov), digits = 4)
 
-# Print influential observations
-print(influential_obs)
-# Confidence intervals for expected values
-conf_int_14 <- predict(lm_model, newdata = auto_subset[14, ], interval = "confidence", level = 0.975)
-conf_int_31 <- predict(lm_model, newdata = auto_subset[31, ], interval = "confidence", level = 0.975)
+###Tables###
+colunas <- c("vars", "n", "sd", "trimmed", "mad", "min", "max", "range", "skew", "kurtosis", "se")
+describe_nosso <- tab_describe[,colunas]
 
-# Prediction intervals
-pred_int_14 <- predict(lm_model, newdata = auto_subset[14, ], interval = "prediction", level = 0.975)
-pred_int_31 <- predict(lm_model, newdata = auto_subset[31, ], interval = "prediction", level = 0.975)
+auto_mat <- cbind(describe_nosso,
+                  Winsorized_Mean = c(auto_win_mpg, auto_win_cylinders, auto_win_displacement, 
+                                      auto_win_horsepower, auto_win_weight, auto_win_acceleration,
+                                      auto_win_year, auto_win_origin),
+                  Variance = cbind(data.matrix(auto_var)))
 
-# Print results
-print(conf_int_14)
-print(conf_int_31)
-print(pred_int_14)
-print(pred_int_31)
+###Graphics###
 
+##Boxplot of all the variables of auto except name
+# Create boxplots for each variable in the data frame
+pdf("boxplots2.pdf", width = 10, height = 5)
+par(mfrow = c(1, ncol(auto_subset)))
+for (col_name in names(auto_subset)) {
+  boxplot(
+    auto_subset[[col_name]],
+    main = col_name, col = "lightpink",
+    border = "black",
+    notch = TRUE
+  )
+  rm(col_name)
+}
+dev.off()
+
+###
+pairs.panels(auto_subset)
+
+#Boxplot of Mahalanobis Distance
+pdf("mahalanobis_distances2.pdf", width = 8, height = 5)
+plot(
+  auto_maha,
+  pch = 16,
+  col = "purple",
+  main = "Mahalanobis Distances",
+  xlab = "Observation",
+  ylab = "Distance"
+)
+abline(
+  h = sqrt(qchisq(0.975, df = ncol(auto_subset))),
+  col = "blue",
+  lty = 2
+)
+dev.off()
+
+
+#2A
+#COMEÇA AQUI, pedir para Manuel explicar no relatório
+reg1 = lm(mpg~cylinders + displacement + horsepower + weight + acceleration + year + origin, data=auto_subset)
+summary(reg1)
+reg2 = lm(mpg~cylinders  + horsepower + weight + acceleration + year + origin, data=auto_subset)
+summary(reg2)
+reg3 = lm(mpg~cylinders  + horsepower + weight + acceleration + origin, data=auto_subset)
+summary(reg3)
+reg4 = lm(mpg~cylinders  + horsepower + weight + acceleration , data=auto_subset)
+summary(reg4)
+reg5 = lm(mpg~cylinders  + weight + acceleration , data=auto_subset)
+summary(reg5)
+#Fazendo este comando e retirando o valor mais alto que n tem * até chegarmos a um output só com asterisco vemos que cylinders  + weight + acceleration sao os selecionados
+reg_mpg <- lm(log(mpg) ~ log(cylinders) + log(weight) + log(acceleration), data = auto_subset)
+summary(reg_mpg)
+#Percentage of variability of the response variable that is explained by the regression model: r²=0.8911, r²adj=0.884 
+multiple_r_squared <- summary(reg_mpg)$r.squared
+adjusted_r_squared <- summary(reg_mpg)$adj.r.squared
+res <- paste(' r² =', multiple_r_squared, '\n','r²adj =', adjusted_r_squared)
+cat(res)
+
+#2B
+
+#2C
+obs_14 <- auto_subset[14,]
+obs_31 <- auto_subset[31,]
+
+##Confidence Interval##
+#14
+p_conf_14 <- predict(reg_mpg, interval='confidence', newdata = obs_14, 0.975)
+p_conf_14
+p_conf_14_lwr_value <- p_conf_14$fit[, "lwr"]
+p_conf_14_upr_value <- p_conf_14$fit[, "upr"]
+vec_p_conf_14 = paste(']' , p_conf_14_lwr_value ,  ' , ' , p_conf_14_upr_value , '[')
+vec_p_conf_14_og = paste(']' , exp(p_conf_14_lwr_value) ,  ' , ' , exp(p_conf_14_upr_value) , '[')
+
+#31
+p_conf_31 <- predict(reg_mpg, interval='confidence', newdata = obs_31, 0.975)
+p_conf_31
+p_conf_31_lwr_value <- p_conf_31$fit[, "lwr"]
+p_conf_31_upr_value <- p_conf_31$fit[, "upr"]
+vec_p_conf_31 = paste(']' , p_conf_31_lwr_value ,  ' , ' , p_conf_31_upr_value , '[')
+vec_p_conf_31_og = paste(']' , exp(p_conf_31_lwr_value) ,  ' , ' , exp(p_conf_31_upr_value) , '[')
+
+##Prediction Interval##
+#14
+p_pred_14 <- predict(reg_mpg, interval='prediction', newdata = obs_14, 0.975)
+p_pred_14
+p_pred_14_lwr_value <- p_pred_14$fit[, "lwr"]
+p_pred_14_upr_value <- p_pred_14$fit[, "upr"]
+vec_p_pred_14 = paste(']' , p_pred_14_lwr_value ,  ' , ' , p_pred_14_upr_value , '[')
+vec_p_pred_14_og = paste(']' , exp(p_pred_14_lwr_value) ,  ' , ' , exp(p_pred_14_upr_value) , '[')
+p_pred_14
+vec_p_pred_14
+vec_p_pred_14_og
+#31
+p_pred_31 <- predict(reg_mpg, interval='prediction', newdata = obs_31, 0.975)
+p_pred_31
+p_pred_31_lwr_value <- p_pred_31$fit[, "lwr"]
+p_pred_31_upr_value <- p_pred_31$fit[, "upr"]
+vec_p_pred_31 = paste(']' , p_pred_31_lwr_value ,  ' , ' , p_pred_31_upr_value , '[')
+vec_p_pred_31_og = paste(']' , exp(p_pred_31_lwr_value) ,  ' , ' , exp(p_pred_31_upr_value) , '[')
+p_pred_31
+vec_p_pred_31
+vec_p_pred_31_og
+
+res_obs_14 <- c(vec_p_conf_14, vec_p_conf_14_og, vec_p_pred_14, vec_p_pred_14_og)
+res_obs_31 <- c(vec_p_conf_31, vec_p_conf_31_og, vec_p_pred_31, vec_p_pred_31_og)
+result_df <- data.frame(Obs_14 = res_obs_14, Obs_31 = res_obs_31)
+rownames(result_df) <- c('CI', 'CI_OG', 'PI', 'PI_OG')
+
+# Displaying the result data frame
+print(result_df)
